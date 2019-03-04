cd452ff2df7228cad968eb29bc9923ae7765c10a
vipingarg7665 <vipingarg7665@gmail.com>
2019-03-04 12:56:03 +0530

    first commit

---
+++ app_server/module/requestdetail.js
@@ -0,0 +1,90 @@
+const mongoose = require('mongoose'); 
+const Schema = mongoose.Schema;
+
+const requestdetails = mongoose.Schema({ 
+
+  requesterdate:{
+      type: String,
+  },
+    department:{
+      type: String,
+    },
+    requestername:{ 
+      type: String,
+    },
+    contact:{
+      type: String,
+   },
+   title:{
+    type: String,
+ },
+   altcontact:{
+      type: String,
+   },
+   requesteremail:{
+      type:String,
+   },
+   noofvisitors:{
+    type: String,
+},
+   gender:{
+    type: String,
+},
+
+Requester:[
+    {
+        firstname:{
+    type: String,
+},
+lastname:{
+    type: String,
+},
+organistation:{
+    type: String,
+},
+contact:{
+    type: String,
+},
+
+email:{
+    type: String,
+}
+}],
+
+arrivaldate:{
+    type: String,
+},
+arrivaltime:{
+    type: String,
+},
+
+purpose:{
+    type: String,
+},
+pickupreq:{
+    type: String,
+},
+pickupinfo:{
+    type: String,
+},
+
+departuredate:{
+    type: String,
+},
+departuretime:{
+    type: String,
+},
+dropreq:{
+    type: String,
+},
+dropinfo:{
+    type: String,
+},
+meals: {
+  type: String,
+},
+
+  
+})
+
+module.exports = mongoose.model('requestdetail',requestdetails );
